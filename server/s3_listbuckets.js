a9300106d89b6fe011a0e2c1508d6aa08e07a5de
Alex Buchan <alex.buchan@telefonica.net>
2017-08-02 11:29:56 +0200

    default .env setup

--- server/s3_listbuckets.js
+++ server/s3_listbuckets.js
@@ -3,7 +3,7 @@
 var AWS = require('aws-sdk');
 
 // Load credentials and set region from JSON file
-AWS.config.loadFromPath('./.env');
+AWS.config.loadFromPath('.env');
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
