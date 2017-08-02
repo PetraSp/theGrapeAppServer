404b70eba6b4296ecd640c7e7c11bb94b0885ddf
Alex Buchan <alex.buchan@telefonica.net>
2017-08-02 10:06:40 +0200

    add dotenv protection

--- server/s3_listbuckets.js
+++ server/s3_listbuckets.js
@@ -1,15 +1,9 @@
 
-// ############# ############# ############# ############# ############# 
-//                º FÑHSFIO¨GSHFIÖGHSIO¨GHIOE¨H
-// ############# ############# ############# ############# ############# 
-
 // Load the SDK for JavaScript
 var AWS = require('aws-sdk');
 
 // Load credentials and set region from JSON file
-AWS.config.loadFromPath('./config.json');
-
-// ############# ############# ############# ############# ############# 
+AWS.config.loadFromPath('./.env');
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
