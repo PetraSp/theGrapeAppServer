0945705dbe17e2269fb0ea8a37280c0cd58233ef
Rachel Renee Flores <rachelreneeflores@gmail.com>
2017-08-03 10:53:20 +0200

    add tasting-entries route and get method

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var wineRoutes = require('./api/wine-entries');
 var userRoutes = require('./api/user-entries');
+var tastingRoutes = require('./api/tasting-entries');
 
 /* GET home page. */
 router.use('/api', wineRoutes);
 router.use('/api', userRoutes);
+router.use('/api', tastingRoutes);
 
 module.exports = router;
