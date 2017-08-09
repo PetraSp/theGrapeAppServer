5d89a87d0be1c55d6d99141fe108bec84e6d0c4c
Alex Buchan <alex.buchan@telefonica.net>
2017-08-09 13:33:13 +0200

    add token to login/signup

--- server/routes/api/user-entries.js
+++ server/routes/api/user-entries.js
@@ -1,7 +1,11 @@
 /*jshint esversion: 6*/
-
-const express       = require('express');
-const router        = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+const passport = require('../../config/passport');
+const jwt = require('jsonwebtoken');
+const jwtOptions = require('../../config/jwt');
 const Entry         = require('../../models/user');
 const User          = require('../../models/user');
 
@@ -23,22 +27,73 @@ router.get('/user-entries/:id', (req, res, next) => {
 });
 
 
-router.post('/user-entries', (req,res,next) => {
-  console.log('my req.body!!!', req.body);
+router.post('/signup', (req,res,next) => {
+  console.log('IM ON THE BACKEND!');
+  console.log('req.body on backend', req.body);
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log('things', username, password);
+  if (!username || !password) {
+      res.status(400).json({ message: "Provide username and password" });
+      return;
+  }
+  User.findOne({ username }, "username", (err, user) => {
+      if (user !== null) {
+          res.status(400).json({ message: 'user exist' });
+          return;
+      }
+      var salt = bcrypt.genSaltSync(bcryptSalt);
+      var hashPass = bcrypt.hashSync(password, salt);
+
   const newUser = new User ({
-    username: req.body.username,
-    password: req.body.password,
+    username,
+    password: hashPass,
     email: req.body.email,
     city: req.body.city,
     avatar: req.body.avatar
   });
 
-  newUser.save((err) => {
-    if (err) { return err; }
-    if (newUser.errors) { return res.status(400).json(newUser); }
-    return res.json(newUser);
-  });
+  newUser.save((err, user) => {
+            if (err) {
+                res.status(400).json({ message: err });
+            } else {
+                var payload = { id: user._id, user: user.username };
+                var token = jwt.sign(payload, jwtOptions.secretOrKey);
+                res.status(200).json({ message: "ok", token: token, user: user });
+                // res.status(200).json(user);
+            }
+        });
+    });
+});
 
+
+router.post('/login', (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (!username || !password) {
+    res.status(401).json({ message: 'Provide username and password' });
+    return;
+  }
+
+  User.findOne({'username': username}, (err, user) => {
+    if (!user) {
+      res.status(401).json({ message: 'The username or password is incorrect' });
+      return;
+    }
+
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (!isMatch) {
+        res.status(401).json({ message: 'The username or password is incorrect' });
+      }
+      else {
+        const payload = {id: user._id, user: user.username};
+        const token = jwt.sign(payload, jwtOptions.secretOrKey);
+
+        res.status(200).json({ token, user });
+      }
+    });
+  });
 });
 
 module.exports = router;
